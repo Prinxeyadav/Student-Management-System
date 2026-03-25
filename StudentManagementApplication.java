9cd485f2e7c562c3ce111b546f0879632c69f0ae
princeyadav <rajprincekumar9955@gmail.com>
2026-03-25 20:27:40 +0530

    Remove DataSourceAutoConfiguration exclusion

--- StudentManagementApplication.java
+++ StudentManagementApplication.java
@@ -4,11 +4,3 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class StudentManagementApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(StudentManagementApplication.class, args);
-    }
-
-}
